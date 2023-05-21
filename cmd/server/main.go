0c7205c5487c6588b5343f6e406c2698986c68e2
IskenT <turanov2901@gmail.com>
2023-05-22 02:40:05 +0900

    db migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/IskenT/go-rest-api/internal/database"
+	log "github.com/sirupsen/logrus"
+)
 
 //Run function for creating l (StartUp)
 func Run() error{
 	fmt.Println("Starting up project")
+	store, err := database.NewDatabase()
+	if err != nil {
+		log.Error("failed to setup connection to the database")
+		return err
+	}
+	err = store.MigrateDB()
+	if err != nil {
+		log.Error("failed to setup database")
+		return err
+	}
 	return nil
 }
 func main() {
